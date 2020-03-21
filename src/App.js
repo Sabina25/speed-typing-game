db56a8d854481a210bceaae6f6593fc1dcc78a2b
Сабина Кадырова <sabina@Sabinas-MacBook-Pro.local>
2020-03-21 18:21:52 +0200

    Speed typing game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState("")
+    const [time, setTime] = useState(3)
+    const [isStart, setIsStart] = useState(false)
+    const [countWords, setCountWords] = useState(0)
+    const [isDisabled, setIsDisabled] = useState(false)
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    function calculateWordCount(text) {
+      const wordsArr = text.trim().split(" ")
+      return wordsArr.filter(word => word !== "").length
+    }
+    
+    function startGame () {
+      setIsStart(true)
+      setTime(3)
+      setText('')
+      setIsDisabled(true)
+    }
+
+    function endGame() {
+      setIsStart(false)
+      setCountWords(calculateWordCount(text))
+      setIsDisabled(false)
+    }
+
+    useEffect (()=> {
+      if (isStart==true && time!==0) {
+        setTimeout(() => {
+          setTime(time=>time-1)
+        }, 1000)
+      } else {
+        endGame()
+      }
+    }, [time, isStart])
+    
+    
+  
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea
+                onChange={handleChange}
+                value={text}
+                disabled={!isDisabled}
+            />
+            <h4>Time remaining: {time}</h4>
+            
+            <button onClick={() => {startGame()}} 
+                    disabled={isDisabled}>Start</button>
+            <h1>Word count: {countWords}</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
